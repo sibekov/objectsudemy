b2579d63c3c5b6cf3e0808987ee60cdfd8577765
sibekov <vusimuzij.sibeko@gmail.com>
2024-12-08 14:50:20 +0000

    create and object with function as one of the elements|

---
+++ myscript.js
@@ -0,0 +1,5 @@
+var myperson = {firstname:'vusi',Surname:'sibeko',kids:2,wife:'beatiful',mycalculas:function(){
+    console.log("this person personal living costs is: "+ 2000*this['kids'])}}
+    
+console.log(myperson["firstname"])
+myperson.mycalculas()
